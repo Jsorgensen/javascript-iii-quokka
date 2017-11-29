5c3cf07c566d8698a39b5da904de660c92d7ad29
Mike Jorgensen <mikeljorgensen@gmail.com>
2017-11-29 11:03:00 -0700

    did stuff

--- objects/ObjectAssignPractice.js
+++ objects/ObjectAssignPractice.js
@@ -18,13 +18,20 @@ var smurf = {
 
 // Make the person a thunder cat by assigning thundercat to person
 
-let pt = Object.assign(person, thundercat)
-pt;
-
+var thunderPerson = Object.assign({}, person, thundercat);
+person;
+thunderPerson;
 // We've permanently modified person, but it's better to make a copy.  So copy person then thundercat into a new empty object
 
 // Make the a copy of a person that is a smurf
-
+var personSmurf = Object.assign({}, person, smurf);
+person;
+personSmurf;
 // Make a copy of a person that is a smurf then a thundercat
-
+var personSmurfThudercat = Object.assign({}, person, smurf, thundercat);
+person;
+personSmurfThudercat;
 // Make a copy of a person that is a thundercat then a smurf
+var personThundercatSmurf = Object.assign({}, person, thundercat, smurf);
+person;
+personThundercatSmurf;
--- objects/forInLoopsPractice.js
+++ objects/forInLoopsPractice.js
@@ -7,10 +7,26 @@ var drink = {
 
 //Loop over drink
 //if the value is 'orange' change it to 'cherry'
+for(key in drink){
+    if(drink[key] === 'orange')
+        drink[key] = 'cherry';
 
+    drink;
+}
 
 
 //Write a function, called 'checkValid', that takes in an object like drink 
 //loop over the object and check if values are falsy
 //return false if any values are falsy
 //return true if all values are truthy
+var checkValid = obj => {
+    for(key in obj){
+        if(!obj[key])
+            return false;
+    }
+
+    return true;
+}
+
+var test = checkValid(drink);
+test;
